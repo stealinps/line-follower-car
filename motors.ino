d0794d6646225d9f04afcd99f7d927f73b09ea82
Stealinp <stealinp@gmail.com>
2026-03-03 14:03:02 +0800

    Implement ultrasonic sensor for motor control
    Added ultrasonic distance sensing functionality to control motor behavior based on distance.

--- motors.ino
+++ motors.ino
@@ -3,18 +3,35 @@ int pinLR=16;
 int pinRF=18;
 int pinRR=19;
 
+int echo=34;
+int trig=5;
+
 
 void setup() {
+  Serial.begin(115200);
   pinMode(pinLF,OUTPUT);
   pinMode(pinLR,OUTPUT);
   pinMode(pinRF,OUTPUT);
   pinMode(pinRR,OUTPUT);
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
 }
 
 void loop() {
+  int distance=ultrasonic();
+  if (distance<10){
+    stop();
+    delay(100);
+    right();
+    delay(100);
+  }
+  else{
+    forward();
+    delay(100);
+  }
 }
 
-void right(int duration){
+void right(){
   digitalWrite(pinLF,LOW);
   digitalWrite(pinLR,HIGH);
   digitalWrite(pinRF,HIGH);
@@ -54,4 +71,31 @@ void test(){
   left();
   delay(1000);
   stop();
+}
+int ultrasonic(){
+  long duration, cm;
+
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+
+  duration = pulseIn(echo, HIGH,30000);
+  cm = microsecondsToCentimeters(duration);
+  
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  delay(10);
+  return cm;
+
+}
+
+long microsecondsToInches(long microseconds) {
+  return microseconds / 74 / 2;
+} 
+
+long microsecondsToCentimeters(long microseconds) {
+  return microseconds / 29 / 2;
 }
